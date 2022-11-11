99d2fdd98b2b76c0caeb888ce9aa9c31066f9568
Adnurhytia <adindanurhayati30@gmail.com>
2022-11-11 17:21:53 +0700

    add the node for inserted by first node

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -29,6 +29,14 @@ namespace singly_linked_list
             Node newnode = new Node();
             newnode.rollNumber = nim;
             newnode.name = nm;
+            //If the node to be inserted is the first node
+            if (START != null || nim <= START.rollNumber)
+            {
+                if ((START != null) && (nim == START.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed\n");
+                }
+            }
         }
     }
 }
